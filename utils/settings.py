2f5f69a2db6afb722786c004f9b206dd248ab623
StarLight1212 <102356230+StarLight1212@users.noreply.github.com>
2024-08-17 22:29:58 +0800

    Update settings.py

--- utils/settings.py
+++ utils/settings.py
@@ -1,8 +1,8 @@
 # Path Loading
 pattern_path = './dataRestore/pattern/Key.csv'
 # Setting weights for Sentence Similarity Model and Chat Model
-ss_weights = r'./model_load/all-MiniLM-L6-v2'
-chat_weights = r"./model_load/researchGPT/7b_v01"
+ss_weights = r'./model_load/all-MiniLM-L6-v2/'
+chat_weights = r"./model_load/story_llm/"
 embeddings_path = r"./model_load/instructor-large/"
 COLLECTION_NAME = 'pdf_buffer'
 PERSISTENCE_DIR = "./dataRestore/PDF_Restore"
