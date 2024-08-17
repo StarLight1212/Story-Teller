a613355c67eb7135db95c844d2f72885807878ae
StarLight1212 <102356230+StarLight1212@users.noreply.github.com>
2024-08-17 21:51:16 +0800

    Add files via upload

---
+++ utils/common_utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+class DFAFilter(object):
+    '''
+    Filter Messages from keywords
+    Use DFA to keep algorithm perform constantly
+    >>> f = DFAFilter()
+    >>> f.add("sexy")
+    >>> f.filter("hello sexy baby")
+    hello **** baby
+    '''
+
+    def __init__(self):
+        self.keyword_chains = {}
+        self.delimit = '\x00'
+
+    def add(self, keyword):
+        # if not isinstance(keyword, unicode):
+        #     keyword = keyword.decode('utf-8')
+        keyword = keyword.lower()
+        chars = keyword.strip()
+        if not chars:
+            return
+        level = self.keyword_chains
+        for i in range(len(chars)):
+            if chars[i] in level:
+                level = level[chars[i]]
+            else:
+                if not isinstance(level, dict):
+                    break
+                for j in range(i, len(chars)):
+                    level[chars[j]] = {}
+                    last_level, last_char = level, chars[j]
+                    level = level[chars[j]]
+                last_level[last_char] = {self.delimit: 0}
+                break
+        else:
+            level[self.delimit] = 0
+
+    def parse(self, path):
+        with open(path) as f:
+            for keyword in f:
+                self.add(keyword.strip())
+
+    def filter(self, message, repl="*"):
+        # if not isinstance(message, unicode):
+        #     message = message.decode('utf-8')
+        ret = []
+        start = 0
+        while start < len(message):
+            level = self.keyword_chains
+            step_ins = 0
+            for char in message[start:]:
+                if char in level:
+                    step_ins += 1
+                    if self.delimit not in level[char]:
+                        level = level[char]
+                    else:
+                        ret.append(repl * step_ins)
+                        start += step_ins - 1
+                        break
+                else:
+                    ret.append(message[start])
+                    break
+            else:
+                ret.append(message[start])
+            start += 1
+
+        return ''.join(ret)
---
+++ utils/settings.py
@@ -0,0 +1,9 @@
+# Path Loading
+pattern_path = './dataRestore/pattern/Key.csv'
+# Setting weights for Sentence Similarity Model and Chat Model
+ss_weights = r'../ResearchGPTv0.1/Professional_QA/receipts/model_load/all-MiniLM-L6-v2'
+chat_weights = r"../ResearchGPTv0.1/Professional_QA/receipts/model_load/researchGPT/7b_v01"
+embeddings_path = r"./model_w/instructor-large/"
+COLLECTION_NAME = 'pdf_buffer'
+PERSISTENCE_DIR = "./dataRestore/PDF_Restore"
+
